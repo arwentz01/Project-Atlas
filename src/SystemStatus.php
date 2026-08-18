3d609e6b842cd84f4603b29904b6caf06f631e26
arwentz01 <arwentz01@gmail.com>
2026-08-18 16:18:50 -0400

    Expand Master Schedule workflow (src/SystemStatus.php)

--- src/SystemStatus.php
+++ src/SystemStatus.php
@@ -21,6 +21,7 @@ final class SystemStatus
         'time_entries',
         'pay_profiles','payroll_exports',
         'master_schedules','master_schedule_entries','master_schedule_generations','master_schedule_generation_items',
+        'master_schedule_versions','schedule_exceptions','master_generation_resolutions','master_generation_publications',
     ];
 
     public static function missingTables(PDO $db): array
